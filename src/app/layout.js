e646156b35ae06b91e277e77f9c00ac55156c814
Vivek Singh <vviveksingh8874@gmail.com>
2026-07-22 18:54:10 +0530

    remove margin

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,10 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full w-full antialiased`}
+      style={{ margin: 0, padding: 0 }}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="h-full w-full m-0 p-0 overflow-hidden flex flex-col">{children}</body>
     </html>
   );
 }
