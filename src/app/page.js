9e75cd1bbc325ccf6a393f9a2566ba678e2886c1
Vivek Singh <vviveksingh8874@gmail.com>
2026-07-23 18:11:33 +0530

    some changes

--- src/app/page.js
+++ src/app/page.js
@@ -116,9 +116,6 @@ export default function Home() {
               <h1 className={styles.mainTitle}>Amfah Expert</h1>
             </div>
           </div>
-          <div className={styles.headerIcons}>
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><polyline points="6 9 12 15 18 9"/></svg>
-          </div>
         </div>
         <div className={styles.onlineStatus}>We are online!</div>
       </header>
